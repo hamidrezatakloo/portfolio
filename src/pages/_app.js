fa20e029369266b1b13d6cec1cffc95a02af3b18
hamidreza <takloohamidreza@gmail.com>
2023-03-01 08:20:47 -0500

    Set primary color in tailwindCss

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return( <div className='bg-primary min-h-screen'>
+  <Component {...pageProps} />
+</div> )
 }
