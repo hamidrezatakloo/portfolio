95ef31297c08f813c299da1a404e2f750c251c90
hamidreza <takloohamidreza@gmail.com>
2023-03-03 08:33:08 -0500

    Add select lang for website in fa and en

--- src/components/Sidemenu.js
+++ src/components/Sidemenu.js
@@ -1,6 +1,10 @@
-import { forwardRef } from "react";
+import { forwardRef, useState } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 const SideMenu = forwardRef(function SideMenu(props, ref) {
+  const router = useRouter();
+  const [drop, setDrop] = useState(false);
+
   const transitionStyles = {
     entering: "w-[180px] h-screen",
     entered: "w-[180px] h-screen",
@@ -24,16 +28,38 @@ const SideMenu = forwardRef(function SideMenu(props, ref) {
           </div>
         </div>
       </div>
-      <ul className="text-2xl font-bold flex flex-col gap-4 items-start">
-        <Link href="/" className="text-secondary">
-          Home
+      <div className="text-2xl font-bold flex flex-col gap-4 items-start">
+        <Link href="/">
+          <span className={router.pathname == "/" ? "text-secondary" : ""}>
+            Home
+          </span>
         </Link>
-        <Link href="/projects">Projects</Link>
-        <Link href="contact">Contact me</Link>
-      </ul>
-      <div className="flex gap-2 mb-4">
+        <Link href="/projects">
+          <span
+            className={router.pathname == "/projects" ? "text-secondary" : ""}
+          >
+            Projects
+          </span>
+        </Link>
+        <Link href="contact">
+          <span
+            className={router.pathname == "/contact" ? "text-secondary" : ""}
+          >
+            Contact me
+          </span>
+        </Link>
+      </div>
+      <div className="flex gap-2 mb-4 relative">
         <span className="text-xl">EN</span>
-        <img src="./arrow down.svg" />
+        <button onClick={() => setDrop(!drop)}>
+          <img src="./arrow down.svg" />
+        </button>
+        {drop && (
+          <div className="bg-white text-lg text-center font-bold text-secondary absolute w-full -top-28 rounded">
+            <div className="border-b-2 border-secondary p-2">Fa</div>
+            <div className=" p-2">En</div>
+          </div>
+        )}
       </div>
     </div>
   );
