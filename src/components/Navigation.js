a8e5945bb78f231b0f30ef1c18b66e9a658ced07
hamidreza <takloohamidreza@gmail.com>
2023-03-03 06:59:46 -0500

    Add transition to sidebar menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,36 +1,21 @@
 import { useState } from "react";
+import SideMenu from "./Sidemenu";
+import { Transition } from "react-transition-group";
+import { useRef } from "react";
+const duration = 500;
 
 const Navigation = () => {
+  const nodeRef = useRef(null);
   const [side, setSide] = useState(false);
   return (
     <nav className="flex justify-between">
       <img src="./Logo.svg" alt="logo" />
-      <button onClick={() => setSide(!side)}>
+      <button onClick={() => setSide(true)}>
         <img src="./Menu.svg" alt="menu" />
       </button>
-      {side && (
-        <div className="absolute  text-white top-0 right-0 bg-[#091939] w-[180px] h-screen flex flex-col justify-between items-center">
-          <div className="mt-4 mx-2 self-stretch flex justify-between">
-            <button onClick={() => setSide(false)}>
-              <img src="./close.svg" />
-            </button>
-            <div className="h-[37px] w-[81px] bg-secondary rounded-[122px] relative">
-              <div className="w-[26px] h-[26px] rounded-full bg-white absolute right-2 top-1">
-                <img src="./Subtract.svg" className="absolute top-1 left-1.5" />
-              </div>
-            </div>
-          </div>
-          <ul className="text-2xl font-bold">
-            <li>Home</li>
-            <li>Projects</li>
-            <li>Contact me</li>
-          </ul>
-          <div className="flex gap-2 mb-4">
-            <span className="text-xl">EN</span>
-            <img src="./arrow down.svg" />
-          </div>
-        </div>
-      )}
+      <Transition nodeRef={nodeRef} in={side} timeout={duration}>
+        {(state) => <SideMenu ref={nodeRef} state={state} setSide={setSide} />}
+      </Transition>
     </nav>
   );
 };
--- src/components/Sidemenu.js
+++ src/components/Sidemenu.js
@@ -1,2 +1,39 @@
-const SideMenu = () => {};
+import { forwardRef } from "react";
+
+const SideMenu = forwardRef(function SideMenu(props, ref) {
+  const transitionStyles = {
+    entering: "w-[180px] h-screen",
+    entered: "w-[180px] h-screen",
+    exiting: "w-0 h-0",
+    exited: "w-0 h-0",
+  };
+  return (
+    <div
+      ref={ref}
+      className={`transition-all overflow-hidden absolute text-white top-0 right-0 bg-[#091939] flex flex-col justify-between items-center ${
+        transitionStyles[props.state]
+      }`}
+    >
+      <div className="mt-4 mx-2 self-stretch flex justify-between">
+        <button onClick={() => props.setSide(false)}>
+          <img src="./close.svg" />
+        </button>
+        <div className="h-[37px] w-[81px] bg-secondary rounded-[122px] relative">
+          <div className="w-[26px] h-[26px] rounded-full bg-white absolute right-2 top-1">
+            <img src="./Subtract.svg" className="absolute top-1 left-1.5" />
+          </div>
+        </div>
+      </div>
+      <ul className="text-2xl font-bold">
+        <li>Home</li>
+        <li>Projects</li>
+        <li>Contact me</li>
+      </ul>
+      <div className="flex gap-2 mb-4">
+        <span className="text-xl">EN</span>
+        <img src="./arrow down.svg" />
+      </div>
+    </div>
+  );
+});
 export default SideMenu;
