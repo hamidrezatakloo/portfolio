45f67385da240152f4e37b5ffb5deac225639734
hamidreza <takloohamidreza@gmail.com>
2023-03-21 08:21:06 -0400

    Detect selected in all components lang with useContext

--- src/components/SelectLang.js
+++ src/components/SelectLang.js
@@ -1,19 +1,34 @@
-import { useState } from "react";
-
+import { useContext, useState } from "react";
+import { langContext } from "@/pages/_app";
 const SelectLang = () => {
   const [drop, setDrop] = useState(false);
+  const { lang, setLang } = useContext(langContext);
   return (
     <div className="flex gap-2 mb-4 sm:mb-0 relative text-white">
-      <span className="text-xl">EN</span>
+      <span className="text-xl text-center w-8">{lang}</span>
       <button onClick={() => setDrop(!drop)}>
         <img src="./arrow down.svg" />
       </button>
       {drop && (
         <div className="bg-white text-lg flex flex-col font-bold text-secondary absolute w-full -top-28 sm:top-10 rounded">
-          <button className="border-b-2 border-secondary p-2 hover:bg-primary">
+          <button
+            className="border-b-2 border-secondary p-2 hover:bg-primary"
+            onClick={() => {
+              setLang("FA");
+              setDrop(false);
+            }}
+          >
             Fa
           </button>
-          <button className="p-2 hover:bg-primary">En</button>
+          <button
+            className="p-2 hover:bg-primary"
+            onClick={() => {
+              setLang("EN");
+              setDrop(false);
+            }}
+          >
+            En
+          </button>
         </div>
       )}
     </div>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,15 @@
 import "@/styles/globals.css";
 import Navigation from "../components/Navigation";
-
+import { createContext, useState } from "react";
+export const langContext = createContext(null);
 export default function App({ Component, pageProps }) {
+  const [lang, setLang] = useState("EN");
   return (
-    <div className="bg-primary min-h-screen p-4 pb-0 sm:pt-12 flex flex-col">
-      <Navigation />
-      <Component {...pageProps} />
-    </div>
+    <langContext.Provider value={{ lang, setLang }}>
+      <div className="bg-primary min-h-screen p-4 pb-0 sm:pt-12 flex flex-col">
+        <Navigation />
+        <Component {...pageProps} />
+      </div>
+    </langContext.Provider>
   );
 }
