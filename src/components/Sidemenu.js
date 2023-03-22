c5791f7a7fd430d0be9108ce2957854d97105418
hamidreza <takloohamidreza@gmail.com>
2023-03-22 07:28:49 -0400

    Add persian support to sideMenu

--- src/components/Sidemenu.js
+++ src/components/Sidemenu.js
@@ -1,8 +1,12 @@
-import { forwardRef } from "react";
+import { forwardRef, useContext } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import SelectLang from "./SelectLang";
+import langData from "./../lang.json";
+import { langContext } from "@/pages/_app";
 const SideMenu = forwardRef(function SideMenu(props, ref) {
+  const { lang } = useContext(langContext);
+
   const router = useRouter();
 
   const transitionStyles = {
@@ -14,9 +18,9 @@ const SideMenu = forwardRef(function SideMenu(props, ref) {
   return (
     <div
       ref={ref}
-      className={`transition-all overflow-hidden absolute text-white top-0 right-0 bg-[#091939] flex flex-col justify-between items-center ${
+      className={`transition-all overflow-hidden absolute text-white top-0 bg-[#091939] flex flex-col justify-between items-center ${
         transitionStyles[props.state]
-      }`}
+      } ${lang === "FA" ? "left-0" : "right-0"}`}
     >
       <div className="mt-4 mx-2 self-stretch flex justify-between">
         <button onClick={() => props.setSide(false)}>
@@ -31,21 +35,21 @@ const SideMenu = forwardRef(function SideMenu(props, ref) {
       <div className="text-2xl font-bold flex flex-col gap-4 items-start">
         <Link href="/">
           <span className={router.pathname == "/" ? "text-secondary" : ""}>
-            Home
+            {langData[lang]["Home"]}
           </span>
         </Link>
         <Link href="/projects">
           <span
             className={router.pathname == "/projects" ? "text-secondary" : ""}
           >
-            Projects
+            {langData[lang]["Projects"]}
           </span>
         </Link>
         <Link href="contact">
           <span
             className={router.pathname == "/contact" ? "text-secondary" : ""}
           >
-            Contact me
+            {langData[lang]["Contact"]}
           </span>
         </Link>
       </div>
