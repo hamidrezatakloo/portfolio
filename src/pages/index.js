5e173a632d15df58aaeff5a682a730d9bc0ba7f2
hamidreza <takloohamidreza@gmail.com>
2023-03-22 09:39:54 -0400

    Correct responsive issue for Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,15 +16,17 @@ export default function Home() {
         dir={lang === "FA" ? "rtl" : "ltr"}
         className="flex flex-col justify-around sm:justify-start flex-1 sm:items-start sm:p-20"
       >
-        <div className="sm:mt-10 flex flex-col justify-center gap-10 sm:gap-20 items-center">
-          <h1 className="text-4xl sm:text-6xl  text-white font-bold leading-[164.5%] sm:leading-normal">
-            {intro[lang][0]} <br className="sm:hidden"></br> {intro[lang][1]}{" "}
+        <div className="flex flex-col justify-center gap-10 lg:gap-20 items-center">
+          <h1 className="text-4xl lg:text-6xl p-8 text-white font-bold leading-[164.5%] sm:leading-normal lg:leading-normal">
+            {intro[lang][0]} <br className="sm:hidden"></br> {intro[lang][1]}
             <br></br>
             {intro[lang][2]}
-            <span className="text-secondary">&nbsp;{intro[lang][3]}</span>
+            <span className="text-secondary inline-block">
+              &nbsp;{intro[lang][3]}
+            </span>
           </h1>
           <div className="w-[280px]">
-            <button className="text-2xl text-white font-bold bg-secondary px-[45px] sm:px-14 sm:py-4 py-[8px] rounded-[2px]">
+            <button className="text-2xl text-white font-bold bg-secondary px-[45px] sm:px-14 sm:py-4 py-[10px] rounded-[2px]">
               {intro["Projects"][lang]}
               <img
                 src="./circle right.svg"
@@ -38,7 +40,7 @@ export default function Home() {
         <img
           src="./bro.svg"
           alt="bro"
-          className={`sm:absolute sm:bottom-0  sm:w-[680px] ${
+          className={`sm:absolute sm:bottom-0 sm:w-[480px] lg:w-[580px] xl:w-[580px] ${
             lang === "EN" ? "right-20" : "left-20"
           }`}
         />
