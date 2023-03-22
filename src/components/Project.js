a49d112e1e1a6bd0659cbf3e066b31c1a6db5df5
hamidreza <takloohamidreza@gmail.com>
2023-03-22 10:05:25 -0400

    Set responsive projects page

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,7 +4,7 @@ import { useContext } from "react";
 const Project = ({ info }) => {
   const { lang } = useContext(langContext);
   return (
-    <div className="bg-[#091939] flex flex-col gap-6 items-center p-8 rounded-[2px] sm:w-3/12">
+    <div className="bg-[#091939] flex flex-col gap-6 items-center p-8 rounded-[2px] sm:w-5/12 lg:w-3/12">
       <h2
         dir={lang === "FA" ? "rtl" : "ltr"}
         className="text-2xl font-bold text-white flex-1"
@@ -14,10 +14,10 @@ const Project = ({ info }) => {
       <p dir={lang === "FA" ? "rtl" : "ltr"} className="text-[#8CAAE4] flex-1">
         {info.desc[lang]}
       </p>
-      <div className="flex justify-around gap-4">
+      <div className="flex justify-center items-center gap-4">
         <a
           href={info.live}
-          className="bg-secondary text-white font-bold text-lg py-1.5 px-5 rounded-[2px]"
+          className="bg-secondary text-white font-bold text-lg p-3 rounded-[2px] text-center"
         >
           {lang === "FA" ? "بازدید کردن" : "Open Live"}
         </a>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,7 +3,7 @@ import projectData from "../projectData";
 const Projects = () => {
   return (
     <main className="flex-1 flex items-center">
-      <div className="px-8 flex flex-col sm:flex-row sm:flex-wrap sm:justify-center sm:w-8/12 mx-auto gap-4 py-10">
+      <div className="px-8 flex flex-col sm:flex-row sm:flex-wrap sm:justify-center 2xl:w-8/12 mx-auto gap-4 py-10">
         {projectData.map((pr, index) => (
           <Project key={index} info={pr} />
         ))}
